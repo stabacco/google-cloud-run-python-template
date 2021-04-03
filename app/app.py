c677622365ede5d9e713d74f5640cf3858ac03d2
Stefano Tabacco <tabacco.stefano@gmail.com>
2021-04-04 00:28:18 +1100

    Wip

--- app/app.py
+++ app/app.py
@@ -1,12 +1,31 @@
 import os, urllib.request, json
 
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask.helpers import send_file
 
-app = Flask(__name__)
+
+def render_template(template_name: str, render_kwargs: "Dict[Any, Any]"):
+    import jinja2
+
+    templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
+    templateEnv = jinja2.Environment(
+        loader=templateLoader, extensions=["jinja2.ext.i18n"]
+    )
+
+    send_file('firebase-messaging-sw.js')
+    template = templateEnv.get_template(template_name)
+    return template.render(render_kwargs)
+
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello_world():
-    return "Hello Stef"
+    return render_template("index.html", {})
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
---
+++ app/static/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAktNMg9BSj6waQvJBIZHvU52qBPRBWZIk",
+    authDomain: "notifications-test-stefano.firebaseapp.com",
+    projectId: "notifications-test-stefano",
+    storageBucket: "notifications-test-stefano.appspot.com",
+    messagingSenderId: "707914582679",
+    appId: "1:707914582679:web:011c79c9b6dd3330fcb7d2",
+    measurementId: "G-GRZSQ1P02K"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const FIREBASE_MESSAGING = firebase.messaging();
+  
